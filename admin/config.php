1dfe899f3ca54549e28d65ce22865cf031fa6b35
anthonyloprimo <anthonyloprimo@gmail.com>
2025-12-31 00:32:38 -0500

    Fixed LICENSE.md, admin panel UI display.

--- admin/config.php
+++ admin/config.php
@@ -181,12 +181,14 @@ if (!empty($usersWarnings)) {
             <input class="headlineInput" type="text" name="siteSubtitle" value="<?php echo htmlspecialchars($headerData['subtitle'] ?? ''); ?>" aria-label="Site Subtitle">
         </div>
         <div class="headerActionStack">
-            <div class="signedInAs"><?php echo htmlspecialchars($_SESSION['auth_user'] ?? ''); ?></div>
-            <form method="post" action="">
-                <input type="hidden" name="action" value="logout">
-                <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token'] ?? ''); ?>">
-                <button class="logoutButton" type="submit">Log Out</button>
-            </form>
+            <div class="headerUserStack">
+                <div class="signedInAs"><?php echo htmlspecialchars($_SESSION['auth_user'] ?? ''); ?></div>
+                <form method="post" action="">
+                    <input type="hidden" name="action" value="logout">
+                    <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token'] ?? ''); ?>">
+                    <button class="logoutButton" type="submit">Log Out</button>
+                </form>
+            </div>
             <div class="headerActions">
                 <button class="helpTutorial iconButton" type="button" aria-label="Help" title="Need help?">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" /></svg>

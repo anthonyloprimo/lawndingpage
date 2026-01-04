b34b56264ef2491a28446dba6aea82650b8b4805
anthonyloprimo <anthonyloprimo@gmail.com>
2026-01-04 00:43:12 -0500

    Tweaked pages

--- admin/config.php
+++ admin/config.php
@@ -531,8 +531,8 @@ if (!empty($usersWarnings)) {
             <h3>FAQ</h3>
             <textarea class="paneEditor" name="faqMarkdown" aria-label="FAQ markdown"><?php echo htmlspecialchars($faqMarkdown); ?></textarea>
         </div>
-        <div class="pane glassConvex" id="events">Public events go here</div>
-        <div class="pane glassConvex" id="donate">donate pane here maybe</div>
+        <div class="pane glassConvex" id="events">Coming soon...</div>
+        <!-- <div class="pane glassConvex" id="donate">donate pane here maybe</div> -->
         <div class="pane glassConvex" id="changelog">
             <?php echo $changelog; ?>
         </div>
@@ -548,7 +548,7 @@ if (!empty($usersWarnings)) {
             <li><a class="navLink" href="#" data-pane="rules" aria-label="Rules" title="Edit Rules"><?php echo lawnding_icon_svg('rules'); ?></a></li>
             <li><a class="navLink" href="#" data-pane="faq" aria-label="FAQ" title="Edit FAQ"><?php echo lawnding_icon_svg('faq'); ?></a></li>
             <li><a class="navLink" href="#" data-pane="events" aria-label="Events" title="Events"><?php echo lawnding_icon_svg('events'); ?></a></li>
-            <li><a class="navLink" href="#" data-pane="donate" aria-label="Donate" title="Donate"><?php echo lawnding_icon_svg('donate'); ?></a></li>
+            <!-- <li><a class="navLink" href="#" data-pane="donate" aria-label="Donate" title="Donate"><?php echo lawnding_icon_svg('donate'); ?></a></li> -->
             <li class="navSeparator" aria-hidden="true"></li>
             <li><a class="navLink" href="#" data-pane="changelog" aria-label="Changelog" title="Changelog"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Changelog</title><path d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M6.12,15.5L9.86,19.24L11.28,17.83L8.95,15.5L11.28,13.17L9.86,11.76L6.12,15.5M17.28,15.5L13.54,11.76L12.12,13.17L14.45,15.5L12.12,17.83L13.54,19.24L17.28,15.5Z" /></svg></a></li>
         </ul>
